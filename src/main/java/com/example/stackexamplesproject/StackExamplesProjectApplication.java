d9cec0e3a1a0da0e48acc7ecad1f1456ff57dba1
Pleshchenko Roman <Roman.PLESCHENKO@raiffeisen.ru>
2022-04-02 19:15:43 +0300

    init commit

---
+++ src/main/java/com/example/stackexamplesproject/StackExamplesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.stackexamplesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackExamplesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackExamplesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stackexamplesproject/StackExamplesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stackexamplesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackExamplesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
